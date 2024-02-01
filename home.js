84196b751a4d82c003d46ebf504604112e7942cc
VRithvik <121178820+VRithvik@users.noreply.github.com>
2024-02-01 18:06:27 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,5 @@
+        function loginRegisterButtonClick() {
+            window.location.href = 'login_page.html';
+        }
+        var headerButton = document.getElementById('headerButton');
+       headerButton.addEventListener('click', loginRegisterButtonClick);
